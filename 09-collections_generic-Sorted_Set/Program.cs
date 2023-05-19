7e1b43806f233e8b0c9b62fdc1137f0054bfbe57
fernandog197 <bluewavesalta@gmail.com>
2023-05-19 17:59:19 -0300

    09-collections_generic-Sorted_Set

---
+++ 09-collections_generic-Sorted_Set/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsGenericSortedSet
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            SortedSet<CPoint> sortedSetPoint = new SortedSet<CPoint>();
+
+            sortedSetPoint.Add(new CPoint(3, 7));
+            sortedSetPoint.Add(new CPoint(8, 11));
+            sortedSetPoint.Add(new CPoint(2, 6));
+
+            foreach (CPoint p in sortedSetPoint)
+            {
+                Console.WriteLine("<<{0}>>", p);
+            }
+
+            Console.WriteLine("- - - - -");
+
+            sortedSetPoint.Add(new CPoint(1, 15));
+            sortedSetPoint.Add(new CPoint(1, 2));
+
+            foreach (CPoint p in sortedSetPoint)
+            {
+                Console.WriteLine("<<{0}>>", p);
+            }
+
+            Console.WriteLine("- - - - -");
+        }
+    }
+}
