47199ce20dbcbe8765d2d70dc28b0babca20de9c
fernandog197 <bluewavesalta@gmail.com>
2023-05-20 11:48:24 -0300

    16-Generic_Delegate

---
+++ 16-Generic_Delegate/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericDelegate
+{
+    class Program
+    {
+        //Creamos el delegado generico
+        public delegate void myDelegate<T> (T p);
+
+        public static void Main ()
+        {
+            //Registramos y usamos el StringHandler
+            myDelegate<string> stringDelegate = new myDelegate<string>(StringHandler);
+            stringDelegate("Esto es un string");
+
+            //Registramos y usamos el DoubleHandler
+            myDelegate<double> doubleDelegate = new myDelegate<double>(DoubleHandler);
+            doubleDelegate(58.7);
+        }
+
+        static void StringHandler (string p)
+        {
+            Console.WriteLine("Uso string como tipo y mi valor es: {0}", p);
+        }
+
+        static void DoubleHandler (double p)
+        {
+            Console.WriteLine("Uso double como tipo y mi valor es: {0}", p);
+        }
+    }
+}
