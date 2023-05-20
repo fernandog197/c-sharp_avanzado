c195468c031e1b9aeb280f499ccdb4ad7d918d58
fernandog197 <bluewavesalta@gmail.com>
2023-05-20 20:07:01 -0300

    18-Func

---
+++ 18-Func/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Func
+{
+    class Program
+    {
+        /*
+            Func funciona de manera similar a Action, solo que este nos permite un tipo de retorno distinto
+            a void
+        */
+        public static void Main ()
+        {
+            int x = 5;
+            int y = 9;
+
+            //Definimos el delegado, el ultimo tipo listado es, siempre, el tipo de retorno
+            Func<int, int, int> delegateFunc = new Func<int, int, int>(Suma);
+
+            //Invocamos e imprimimos el resultado
+            Console.WriteLine(delegateFunc(x, y));
+        }
+
+        //Declaramos el handler
+        static int Suma (int a, int b)
+        {
+            int r = 0;
+            r = a + b;
+            return r;
+        }
+    }
+}
