673056c3a3f9fb515ac25b0b14823c45eff38963
fernandog197 <bluewavesalta@gmail.com>
2023-05-29 22:40:04 -0300

    38-System.Configuration -> Algo no funciona. Error de versiones?

---
+++ 38-System.Configuration/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Configuration;
+
+namespace SystemConfiguration
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            //Editamos App.config y lo leemos desde la aplicacion
+
+            //Creamos el objeto lector
+        }
+    }
+}
