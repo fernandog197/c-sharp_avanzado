0e5b6edebc75ffecdc184a3d2cc86c2f54601099
fernandog197 <bluewavesalta@gmail.com>
2023-05-30 16:20:03 -0300

    42-Attribute-Library

---
+++ 42-Attribute-Library/Aritmetica/Class1.cs
@@ -0,0 +1,79 @@
+﻿namespace AritmeticaAttribute
+{
+    [Datos("Mi clase Matematicas")]
+    public class Operaciones
+    {
+        private double a;
+        private double b;
+        private double r;
+
+        public Operaciones (double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+            this.r = 0.0;
+        }
+
+        public double R { get => this.r; }
+
+        public double Suma ()
+        {
+            this.r = this.a + this.b;
+            return R;
+        }
+
+        public double Resta ()
+        {
+            this.r = this.a - this.b;
+            return R;
+        }
+
+        public double Multiplicacion ()
+        {
+            this.r = this.a * this.b;
+            return R;
+        }
+
+        public double Division ()
+        {
+            this.r = this.a / this.b;
+            return R;
+        }
+    }
+
+    [Datos("Clase prueba 01")]
+    public class CPrueba01
+    {
+        public CPrueba01()
+        {
+            Console.WriteLine("Version 01");
+        }
+    }
+
+    [Datos("Clase prueba 02")]
+    public class CPrueba02
+    {
+        public CPrueba02()
+        {
+            Console.WriteLine("Version 02");
+        }
+    }
+
+    //Clase para el Attribute
+    public sealed class DatosAttribute : System.Attribute
+    {
+        string dato;
+
+        public string Dato { get => this.dato; set => this.dato = value; }
+
+        public DatosAttribute ()
+        {
+            this.dato = "";
+        }
+
+        public DatosAttribute (string dato)
+        {
+            this.dato = dato;
+        }
+    }
+}
---
+++ 42-Attribute-Library/Prueba/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using AritmeticaAttribute;
+
+namespace AttributeLibraryPrueba
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            //Usaremos Reflection con early binding
+
+            //Obtenemos el tipo de la clase
+            Type t = typeof(Operaciones);
+
+            //Obtenemos la lista de atributos
+            object[] atributos = t.GetCustomAttributes(false);
+
+            //Mostramos los atributos
+            foreach(DatosAttribute atributo in atributos)
+            {
+                Console.WriteLine("{0}", atributo.Dato);
+            }
+        }
+    }
+}
